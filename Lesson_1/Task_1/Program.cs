8bb629bb188ef110678c144a9a49f0876f7e7809
arslan08 <arslan.08@mail.ru>
2022-08-20 16:07:04 +0500

    Задача №1

---
+++ Lesson_1/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿string numberStrA;
+string numberStrB;
+Console.WriteLine("Введите первое число:");
+numberStrA = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+numberStrB = Console.ReadLine();
+int numberA = int.Parse(numberStrA);
+int numberB = int.Parse(numberStrB);
+if (numberA / numberB == numberB)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
