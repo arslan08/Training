39df12376721bc81108def7dbc8a1af37e5b46d7
arslan08 <arslan.08@mail.ru>
2022-08-21 10:52:52 +0500

    Задача №4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int d = a;
+if (b > d)
+{
+    d = b;
+}
+if (c > d)
+{
+    d = c;
+}
+Console.WriteLine("max = " + d);
