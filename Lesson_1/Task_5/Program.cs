841ab3f87e9a77c58200582b7fccef4cb8c6f37f
arslan08 <arslan.08@mail.ru>
2022-08-21 09:57:07 +0500

    Задача №5

---
+++ Lesson_1/Task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+for (int i = n * -1; i <= n; i++)
+{
+    if (i < n)
+    {
+        Console.Write(i + ", ");
+    }
+    else
+    {
+        Console.Write(i);
+    }
+
+}
