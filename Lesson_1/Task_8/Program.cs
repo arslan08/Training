432c9fca01f68c54e94dfee45ec9ab6dad92cbd2
arslan08 <arslan.08@mail.ru>
2022-08-21 11:19:47 +0500

    Задача №8

---
+++ Lesson_1/Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    if (i < n & i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    if (i == n & i % 2 == 0)
+    {
+        Console.Write(i);
+    }
+}
