a29f4bb642e227ec107ef6e1376e90334352745c
arslan08 <arslan.08@mail.ru>
2022-08-23 19:48:17 +0500

    Задача №12

---
+++ Lesson_2/Task_12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вотрое число");
+int b = int.Parse(Console.ReadLine());
+while (b == 0)
+{
+    Console.WriteLine("Введите число не кратное 0");
+    b = int.Parse(Console.ReadLine());
+}
+if (a % b == 0)
+{
+    Console.WriteLine(a + ", " + b + " -> кратно");
+}
+else
+{
+    Console.WriteLine(a + ", " + b + " -> не кратно, остаток " + a % b);
+}
