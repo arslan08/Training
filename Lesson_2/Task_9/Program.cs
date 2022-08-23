81440b217d3cd40eb4f6345f606fc060ba9b954f
arslan08 <arslan.08@mail.ru>
2022-08-23 11:04:20 +0500

    Задача №9

---
+++ Lesson_2/Task_9/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = new Random().Next(10, 100);
+int a = num / 10;
+int b = num % 10;
+int max = a;
+if (a < b) max = b;
+Console.WriteLine("Максимальная цифра в числе " + num + " -> " + max);
