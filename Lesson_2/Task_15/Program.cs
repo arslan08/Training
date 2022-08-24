e4e91bc35864a4938d830cc7561b48ac0c4c1f61
arslan08 <arslan.08@mail.ru>
2022-08-24 11:16:00 +0500

    Задача №15

---
+++ Lesson_2/Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int num = 0;
+while (num <= 7 ^ num > 0)
+{
+    Console.WriteLine("введите цифру равную дню недели");
+    num = int.Parse(Console.ReadLine());
+}
+if (num == 6 ^ num == 7)
+{
+    Console.WriteLine(num + " -> Да");
+}
+else
+{
+    Console.WriteLine(num + " -> Нет");
+}
