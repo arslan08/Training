bab5d25ed18f57253d47951a65b5e6bf344a08cd
arslan08 <arslan.08@mail.ru>
2022-08-24 11:08:04 +0500

    Задача №13

---
+++ Lesson_2/Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num / 100 > 0)
+{
+    int numTmp = num;
+    while (numTmp / 1000 > 0)
+    {
+        numTmp = numTmp / 10;
+    }
+
+    Console.WriteLine(num + " -> " + (numTmp % 10));
+}
+else
+{
+    Console.WriteLine(num + " -> третьей цифры нет");
+}
