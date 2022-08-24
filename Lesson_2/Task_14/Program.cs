dd7089571cdb5c764d4729da9e6662c80f284e8e
arslan08 <arslan.08@mail.ru>
2022-08-24 08:51:10 +0500

    Задача №14

---
+++ Lesson_2/Task_14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine(a + " -> Да");
+}
+else
+{
+    Console.WriteLine(a + " -> Нет");
+}
