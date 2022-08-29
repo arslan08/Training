a15c0caaf2048fbfca1b6c01b04e9e89ce442215
arslan08 <arslan.08@mail.ru>
2022-08-29 21:46:04 +0500

    Задача №18

---
+++ Lesson_3/Task_18/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void chetverc(string coordX, string coordY)
+{
+    Console.WriteLine("x " + coordX + " 0, " + "y " + coordY + " 0");
+}
+
+Console.WriteLine("Введите номер четверти");
+int x = int.Parse(Console.ReadLine());
+if (x == 1)
+{
+    chetverc(">", ">");
+}
+if (x == 2)
+{
+    chetverc("<", ">");
+}
+if (x == 3)
+{
+    chetverc("<", "<");
+}
+if (x == 4)
+{
+    chetverc(">", "<");
+}
