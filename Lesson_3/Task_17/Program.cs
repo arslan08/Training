c7c86d2c28d569b310df5e52bbb5eaebdfcee523
arslan08 <arslan.08@mail.ru>
2022-08-29 21:33:58 +0500

    задача №17

---
+++ Lesson_3/Task_17/Program.cs
@@ -0,0 +1,16 @@
+﻿int x = 0;
+int y = 0;
+while (x == 0)
+{
+    Console.WriteLine("Введите координаты точки X не равной 0");
+    x = int.Parse(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.WriteLine("Введите координаты точки Y не равной 0");
+    y = int.Parse(Console.ReadLine());
+}
+if (x > 0 && y > 0) Console.WriteLine("Плоскость №1");
+if (x < 0 && y > 0) Console.WriteLine("Плоскость №2");
+if (x < 0 && y < 0) Console.WriteLine("Плоскость №3");
+if (x > 0 && y < 0) Console.WriteLine("Плоскость №4");
