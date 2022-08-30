745868612d3ba760dfd1bf9ca69aa40d1ab4a0e1
arslan08 <arslan.08@mail.ru>
2022-08-30 09:27:24 +0500

    Задача №23

---
+++ Lesson_3/Task_23/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 0;
+if (n == 0)
+{
+    Console.WriteLine("Введите число");
+    n = int.Parse(Console.ReadLine());
+}
+Console.Write(n + "-> ");
+for (int i = 1; i <= n; i++)
+{
+    if (i < n) Console.Write(Math.Pow(i, 3) + ", ");
+    if (i == n) Console.WriteLine(Math.Pow(i, 3));
+}
