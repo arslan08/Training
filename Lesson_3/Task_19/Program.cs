1c29e90723ea3c6c5df5e94e5a73b662d879fec4
arslan08 <arslan.08@mail.ru>
2022-08-30 08:34:06 +0500

    Задача №19

---
+++ Lesson_3/Task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿int Pal = 0;
+bool PalCheck = false;
+while (Pal / 10000 < 1 ^ Pal / 10000 > 10)
+{
+    Console.WriteLine("Введите пятизначное число");
+    Pal = int.Parse(Console.ReadLine());
+}
+for (int i = 10; i <= 100000; i = i * 10)
+{
+    if (Pal % i / (i / 10) == Pal / (100000 / i) % 10)
+    {
+        PalCheck = true;
+    }
+    else
+    {
+        PalCheck = false;
+        break;
+    };
+}
+switch (PalCheck)
+{
+    case true:
+        Console.WriteLine(Pal + " -> да");
+        break;
+    case false:
+        Console.WriteLine(Pal + " -> нет");
+        break;
+}
