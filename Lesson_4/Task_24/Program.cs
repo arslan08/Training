4c4f16b475c93fa9ee889207141650d5daf10da3
arslan08 <arslan.08@mail.ru>
2022-09-04 19:44:10 +0500

    Задача №24

---
+++ Lesson_4/Task_24/Program.cs
@@ -0,0 +1,9 @@
+﻿int GetSumNums(int num)
+{
+    int a = 0;
+    for (int i = 1; i <= num; i++) a += i;
+    return a;
+}
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(GetSumNums(a));
