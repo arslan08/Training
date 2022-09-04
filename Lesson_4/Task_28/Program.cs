89cf857e4b8521ef45bbae2ebb690a58c00cb4d5
arslan08 <arslan.08@mail.ru>
2022-09-04 20:10:26 +0500

    Задача №28

---
+++ Lesson_4/Task_28/Program.cs
@@ -0,0 +1,9 @@
+﻿int GetProisvNum(int num)
+{
+    int tmp = 1;
+    for (int i = 1; i <= num; i++) tmp *= i;
+    return tmp;
+}
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(GetProisvNum(n));
