3b1c13228c4c5077efaddef57255483c9291c10f
arslan08 <arslan.08@mail.ru>
2022-09-04 20:18:44 +0500

    Задача №25

--- Lesson_4/Task_25/Program.cs
+++ Lesson_4/Task_25/Program.cs
@@ -1,4 +1,11 @@
-﻿
-Console.WriteLine("Введите число:");
-string n = Console.ReadLine();
-Console.WriteLine(n.Length);
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = int.Parse(Console.ReadLine());
+int tmp = 1;
+for (int i = 0; i < b; i++)
+{
+    tmp *= a;
+}
+Console.WriteLine(tmp);
