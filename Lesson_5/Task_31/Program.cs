fe44d88a646ad081910e12d8c85793fcaa6b15c6
arslan08 <arslan.08@mail.ru>
2022-09-05 22:09:44 +0500

    Задача №31

---
+++ Lesson_5/Task_31/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int[12];
+int PosSumNum = 0;
+int NegSumNum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-9, 9);
+    PosSumNum += arr[i] < 0 ? arr[i] : 0;
+    NegSumNum += arr[i] > 0 ? arr[i] : 0;
+}
+
+Console.WriteLine($"в массиве [{string.Join(",", arr)}] cумма положительных чисел равна {PosSumNum}, сумма отрицательных чисел равна {NegSumNum}");
