4eed6ea737f03225161680c60963e5936555e147
arslan08 <arslan.08@mail.ru>
2022-09-06 21:52:45 +0500

    Задача №35

---
+++ Lesson_5/Task_35/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int[new Random().Next(0, 123)];
+int ChekNum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 123);
+}
+foreach (int item in arr)
+{
+    if (item > 10 & item < 99) ChekNum += 1;
+}
+Console.WriteLine($"[{string.Join(", ", arr)}] -> {ChekNum}");
