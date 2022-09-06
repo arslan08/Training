c4bfd767668a64d2c12fbc7befccf5a8d90da1ae
arslan08 <arslan.08@mail.ru>
2022-09-06 22:24:50 +0500

    Задача №38

---
+++ Lesson_5/Task_38/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = new int[new Random().Next(2, 10)];
+int ChekNum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 100);
+}
+int min = arr[0];
+int max = arr[0];
+foreach (int item in arr)
+{
+    if (item < min) min = item;
+    if (item > max) max = item;
+}
+Console.WriteLine($"[{string.Join(", ", arr)}] -> {(min + max)}");
