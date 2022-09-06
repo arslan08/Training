1745ace4e540ae6d0f8b91b96aaa12e857724b27
arslan08 <arslan.08@mail.ru>
2022-09-06 17:29:20 +0500

    Задача №32

---
+++ Lesson_5/Task_32/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] Posm = new int[4];
+int[] Neg = new int[4];
+for (int i = 0; i < Posm.Length; i++)
+{
+    Posm[i] = new Random().Next(-9, 9);
+    Neg[i] = Posm[i] * -1;
+
+}
+
+Console.WriteLine($"[{string.Join(",", Posm)}] -> [{string.Join(",", Neg)}] ");
