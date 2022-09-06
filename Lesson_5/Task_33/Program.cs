09fafd0ad1a7850f0c2a0d34852fed85f33cc35e
arslan08 <arslan.08@mail.ru>
2022-09-06 21:16:51 +0500

    Задача №33

---
+++ Lesson_5/Task_33/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = new int[5];
+string ChekNum = "Нет";
+int RandNum = new Random().Next(-9, 9);
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-9, 9);
+}
+foreach (int item in arr)
+{
+    if (item == RandNum) ChekNum = "Да";
+}
+Console.WriteLine($"{RandNum},  массив [{string.Join(", ", arr)}] -> {ChekNum}");
