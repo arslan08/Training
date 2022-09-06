3e36b99fb74714b35ee1f2c430b5ab590bfa4e38
arslan08 <arslan.08@mail.ru>
2022-09-06 22:33:47 +0500

    Задача №30

---
+++ Lesson_5/Task_30/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int[12];
+int Pos = 0;
+int Neg = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-9, 9);
+    if (arr[i] < 0) Pos += arr[i];
+    else Neg += arr[i];
+}
+
+Console.WriteLine($"в массиве [{string.Join(",", arr)}] cумма положительных чисел равна {Pos}, сумма отрицательных чисел равна {Neg}");
