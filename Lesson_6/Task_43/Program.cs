8da9e734a3bc993335a2f79029cb21426724a024
arslan08 <arslan.08@mail.ru>
2022-09-07 23:04:52 +0500

    Задача №43

---
+++ Lesson_6/Task_43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите b1:");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите k1:");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2:");
+double b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите k2:");
+double k2 = int.Parse(Console.ReadLine());
+if (b1 != b2 & k1 != k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+else
+{
+    Console.WriteLine($"Прямые не пересекаються");
+}
+
