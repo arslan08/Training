f01e1d85124b95880d806a161753a0b23fcc0320
arslan08 <arslan.08@mail.ru>
2022-09-07 20:51:00 +0500

    Задача №44

---
+++ Lesson_6/Task_44/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+int[] Fibo = new int[N];
+for (int i = 0; i < Fibo.Length; i++)
+{
+    if (i == 0) Fibo[i] = 0;
+    if (i == 1) Fibo[i] = 1;
+    if (i >= 2) Fibo[i] = (Fibo[i - 1]) + (Fibo[i - 2]);
+}
+Console.WriteLine($"Если N = {N} -> {string.Join(" ", Fibo)}");
