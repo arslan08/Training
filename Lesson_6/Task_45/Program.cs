ea381a0818bcabfa2d3b63ecb794aa43c106640e
arslan08 <arslan.08@mail.ru>
2022-09-07 21:07:02 +0500

    Задача №45

---
+++ Lesson_6/Task_45/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] GetArr(int Size, int Min, int Max)
+{
+    int[] arr = new int[Size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(Min, Max + 1);
+    }
+    return arr;
+}
+
+int[] CopyArr(int[] arr)
+{
+    int[] tmpArr = new int[arr.Length];
+    int i = 0;
+    foreach (var item in arr)
+    {
+        tmpArr[i] = item;
+        Console.WriteLine($"[{i}] -> {item}");
+        i += 1;
+    }
+    return tmpArr;
+}
+int[] arr = GetArr(10, 0, 10);
+Console.WriteLine($"[{string.Join(", ", arr)}] -> [{string.Join(", ", CopyArr(arr))}]");
