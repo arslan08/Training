222e6ac8485e11ded059c499ce686c5a4af28f91
arslan08 <arslan.08@mail.ru>
2022-09-07 12:54:42 +0500

    Задача №40

---
+++ Lesson_6/Task_40/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число №1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число №2:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число №3:");
+int c = int.Parse(Console.ReadLine());
+
+if (a <= b + c & b <= a + c & c <= a + b) Console.WriteLine($"Треугольник со сторонами A={a}, B={b}, C ={c} может существовать");
+else Console.WriteLine($"Треугольник со сторонами A={a}, B={b}, C ={c} не может существовать");
