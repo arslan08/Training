7ce6dac23fc883dc263c0ab9b4c5fbe7a713559c
arslan08 <arslan.08@mail.ru>
2022-09-07 11:29:40 +0500

    Задача №39

---
+++ Lesson_6/Task_39/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = new int[new Random().Next(0, 10)];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+}
+Console.Write($"[{string.Join(", ", arr)}] -> ");
+for (int i = 0; i < arr.Length / 2; i++)
+{
+    int tmp = arr[i];
+    arr[i] = arr[arr.Length - 1 - i];
+    arr[arr.Length - 1 - i] = tmp;
+}
+Console.Write($"[{string.Join(", ", arr)}]");
