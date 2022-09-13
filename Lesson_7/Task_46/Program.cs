05b0316c409298b27a86abcaa2d730f840f64ed9
arslan08 <arslan.08@mail.ru>
2022-09-13 09:00:34 +0500

    Задача №46

---
+++ Lesson_7/Task_46/Program.cs
@@ -0,0 +1,33 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] result = GetArray(rows, columns, 0, 9);
+PrintArray(result);
