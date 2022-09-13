f22c1ae4f22c8e1fd46ad60c6131648c911a312d
arslan08 <arslan.08@mail.ru>
2022-09-13 11:04:48 +0500

    Задача №47

---
+++ Lesson_7/Task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int rows = new Random().Next(1, 10);
+int columns = new Random().Next(2, 10);
+double[,] result = GetArray(rows, columns);
+PrintArray(result);
