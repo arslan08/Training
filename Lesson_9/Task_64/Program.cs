9bb08cd730b6ff9c61b65f660f4a3ff853e01c6f
arslan08 <arslan.08@mail.ru>
2022-09-16 15:08:21 +0500

    Задача №64

---
+++ Lesson_9/Task_64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"M = {m}; N = {n}. -> \"\"");
+for (int i = m; i < n; i++)
+{
+    Console.Write($"{i}, ");
+}
+Console.Write($"{n}\"\"");
