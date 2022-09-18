f328e3a928a46891b88c9ed53718dfc3bf8962a9
arslan08 <arslan.08@mail.ru>
2022-09-18 15:31:09 +0500

    Задача №64

--- Lesson_9/Task_64/Program.cs
+++ Lesson_9/Task_64/Program.cs
@@ -3,8 +3,15 @@ int m = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите N:");
 int n = int.Parse(Console.ReadLine());
 Console.Write($"M = {m}; N = {n}. -> \"\"");
-for (int i = m; i < n; i++)
+
+void summ(int m, int n)
 {
-    Console.Write($"{i}, ");
+    if (m < n)
+    {
+        Console.Write($"{m}, ");
+        summ(m + 1, n);
+    }
 }
+
+summ(m, n);
 Console.Write($"{n}\"\"");
