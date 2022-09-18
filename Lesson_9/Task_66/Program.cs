4101de3b5effcd1b085627f9df07c38e00dd2623
arslan08 <arslan.08@mail.ru>
2022-09-18 11:04:52 +0500

    Задача №66

---
+++ Lesson_9/Task_66/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"M = {m}; N = {n} -> ");
+int temp = m;
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+sumNaturalElements(m, n, temp = 0);
+
+void sumNaturalElements(int m, int n, int NuberSumm)
+{
+    NuberSumm = NuberSumm + n;
+    if (n <= m)
+    {
+        Console.Write($"{NuberSumm} ");
+        return;
+    }
+    sumNaturalElements(m, n - 1, NuberSumm);
+}
